4a246eb3bf6a8cbe13551dbcac191b35bc30cff9
amaramci <amarmujezinovic1@gmail.com>
2024-11-10 19:09:50 +0100

    first

---
+++ src/controler/Controler.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controler;
+
+import java.util.ArrayList;
+import java.util.List;
+import model.Autor;
+import model.Knjiga;
+import model.Zanr;
+
+/**
+ *
+ * @author Korisnik
+ */
+public class Controler {
+
+    private List<Knjiga> listaKnjiga = new ArrayList<>();
+    private List<Autor> listaAutora = new ArrayList<>();
+    
+    private static Controler instance;
+    
+    public static Controler getInstance(){
+        if (instance == null){
+            instance = new Controler();
+        }
+        return instance;
+    }
+
+    private Controler() {
+        Autor autor1 = new Autor("Ivo", "Andric", 1892, "Bigrafija Iva Andrica");
+        Autor autor2 = new Autor("Danilo", "Kis", 1935, "Bigrafija Danila Kisa");
+        Autor autor3 = new Autor("Mesa", "Selimovic", 1910, "Bigrafija Mese Selimovica");
+
+        Knjiga knjiga1 = new Knjiga("Na Drini cuprija", autor1, "1234567890", 1945, Zanr.ISTORIJSKI);
+        Knjiga knjiga2 = new Knjiga("Basta, pepeo", autor2, "0123456789", 1982, Zanr.POEZIJA);
+        Knjiga knjiga3 = new Knjiga("Tvrdjava", autor3, "9012345678", 1970, Zanr.DETEKTIVSKI);
+
+        listaKnjiga.add(knjiga1);
+        listaKnjiga.add(knjiga2);
+        listaKnjiga.add(knjiga3);
+
+        listaAutora.add(autor1);
+        listaAutora.add(autor2);
+        listaAutora.add(autor3);
+
+    }
+
+    public List<Knjiga> getListaKnjiga() {
+        return listaKnjiga;
+    }
+
+    public void setListaKnjiga(List<Knjiga> listaKnjiga) {
+        this.listaKnjiga = listaKnjiga;
+    }
+
+    public List<Autor> getListaAutora() {
+        return listaAutora;
+    }
+
+    public void setListaAutora(List<Autor> listaAutora) {
+        this.listaAutora = listaAutora;
+    }
+
+    public void obrisiKnjigu(int selektovaniRed) {
+        listaKnjiga.remove(selektovaniRed);
+    }
+
+    public void dodajKnjigu(Knjiga novaKnjiga) {
+        listaKnjiga.add(novaKnjiga);
+    }
+
+    
+    
+
+}
---
+++ src/model/Knjiga.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Korisnik
+ */
+public class Knjiga {
+
+    private String naslov;
+    private Autor autor;
+    private String ISBN;
+    private int godinaIzdanja;
+    private Zanr zanr;
+
+    public Knjiga() {
+    }
+
+    public Knjiga(String naslov, Autor autor, String ISBN, int godinaIzdanja, Zanr zanr) {
+        this.naslov = naslov;
+        this.autor = autor;
+        this.ISBN = ISBN;
+        this.godinaIzdanja = godinaIzdanja;
+        this.zanr = zanr;
+    }
+    
+    
+    
+
+    public String getNaslov() {
+        return naslov;
+    }
+
+    public void setNaslov(String naslov) {
+        this.naslov = naslov;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public Zanr getZanr() {
+        return zanr;
+    }
+
+    public void setZanr(Zanr zanr) {
+        this.zanr = zanr;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public int getGodinaIzdanja() {
+        return godinaIzdanja;
+    }
+
+    public void setGodinaIzdanja(int godinaIzdanja) {
+        this.godinaIzdanja = godinaIzdanja;
+    }
+
+    @Override
+    public String toString() {
+        return "Knjiga{" + "naslov=" + naslov + ", autor=" + 
+                autor + ", ISBN=" + ISBN + ", godinaIzdanja=" + 
+                godinaIzdanja + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Knjiga other = (Knjiga) obj;
+        if (this.godinaIzdanja != other.godinaIzdanja) {
+            return false;
+        }
+        if (!Objects.equals(this.naslov, other.naslov)) {
+            return false;
+        }
+        if (!Objects.equals(this.ISBN, other.ISBN)) {
+            return false;
+        }
+        return Objects.equals(this.autor, other.autor);
+    }
+
+}
